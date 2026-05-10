641c2d7466e36a7ad5aef2fe2c05b3571f066b73
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-10 22:53:53 +0500

    Parol yozishda backspace xususiyati qo'shildi

--- Program.cs
+++ Program.cs
@@ -19,6 +19,34 @@ namespace SecureAdminSystem
                 Console.WriteLine("Siz tizimga admin sifatida kirdingiz.");
                 Console.ResetColor();
 
+                Console.WriteLine("1. Foydalanuvchilar ro'yxati");
+                Console.WriteLine("2. Sozlamalar");
+                Console.WriteLine("3. Hisobotlar");
+                Console.WriteLine("4. Chiqish");
+
+                Console.Write("Kerakli bo'limni tanlang: ");
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        {
+                            break;
+                        }
+                    case 2:
+                        {
+                            break;
+                        }
+                    case 3:
+                        {
+                            break;
+                        }
+                    case 4:
+                        {
+                            return;
+                            break;
+                        }
+                }
             }
             else
             {
@@ -63,9 +91,18 @@ namespace SecureAdminSystem
 
                 if (key.Key == ConsoleKey.Enter)
                     continue;
-
-                password += key.KeyChar;
-                Console.Write("*");
+                else if (key.Key == ConsoleKey.Backspace && password != string.Empty)
+                {
+                    password = password.Remove(password.Length-1);
+                    Console.SetCursorPosition(38 + password.Length, 1);
+                    Console.Write(" ");
+                    Console.SetCursorPosition(38 + password.Length, 1);
+                }
+                else
+                {
+                    password += key.KeyChar;
+                    Console.Write("*");
+                }
 
             }while (key.Key != ConsoleKey.Enter);
 
